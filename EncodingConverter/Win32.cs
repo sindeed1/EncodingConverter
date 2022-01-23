098471dbcbdfec32c33c76cf24a2f0a3b1f29268
Ammar Alhamwi <ammareus@gmail.com>
2022-01-23 15:52:39 +0100

    Added ToDo to the branch> Task of the current branch: Add support to console.

---
+++ EncodingConverter/Win32.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace EncodingConverter
+{
+    static internal class Win32Helper
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+    }
+}
