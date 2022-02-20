31731ff9c7178f36f598fd923630366c16be7478
Ammar Alhamwi <ammareus@gmail.com>
2022-02-20 14:51:10 +0100

    Added UnitTest project Minor changes

--- EncodingConverter/Commands/ConsoleCommand.cs
+++ EncodingConverter/Commands/ConsoleCommand.cs
@@ -7,8 +7,6 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.ComponentModel;
-
 namespace EncodingConverter.Commands
 {
     class ConsoleCommand : ICommandLineCommand
@@ -23,11 +21,11 @@ namespace EncodingConverter.Commands
         public bool Execute(string[] args, int argsStartIndex)
         {
             Console.WriteLine("Starting console...");
-            AllocConsole();
-            IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            Win32Helper.AllocConsole();
+            IntPtr stdHandle = Win32Helper.GetStdHandle(Win32Helper.STD_OUTPUT_HANDLE);
             SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, true);
             FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
-            Encoding encoding = System.Text.Encoding.GetEncoding(MY_CODE_PAGE);
+            Encoding encoding = System.Text.Encoding.GetEncoding(Win32Helper.MY_CODE_PAGE);
             StreamWriter standardOutput = new StreamWriter(fileStream, encoding);
             standardOutput.AutoFlush = true;
             Console.SetOut(standardOutput);
@@ -94,112 +92,7 @@ namespace EncodingConverter.Commands
             return true;
         }
 
-        //[DllImport("kernel32.dll", SetLastError = true)]
-        //[return: MarshalAs(UnmanagedType.Bool)]
-        //public static extern bool AllocConsole();
 
-        [DllImport("kernel32.dll",
-            EntryPoint = "GetStdHandle",
-            SetLastError = true,
-            CharSet = CharSet.Auto,
-            CallingConvention = CallingConvention.StdCall)]
-        private static extern IntPtr GetStdHandle(int nStdHandle);
-        [DllImport("kernel32.dll",
-            EntryPoint = "AllocConsole",
-            SetLastError = true,
-            CharSet = CharSet.Auto,
-            CallingConvention = CallingConvention.StdCall)]
-        private static extern int AllocConsole();
-        private const int STD_OUTPUT_HANDLE = -11;
-        private const int MY_CODE_PAGE = 437;
-
-        string[] ConvertToArguments(string line)
-        {
-            const char escapeChar = '\\';
-            const char quoteChar = '"';
-
-            List<string> arguments = new List<string>();
-
-            string arg = string.Empty;
-            bool includeWhiteSpace = false;
-            bool escape = false;
-
-            foreach (var item in line)
-            {
-                if (char.IsWhiteSpace(item))
-                {
-                    if (!includeWhiteSpace)
-                    {
-                        if (!string.IsNullOrEmpty(arg))
-                        {
-                            arguments.Add(arg);
-                        }
-                        arg = string.Empty;
-                        continue;
-                    }
-                }
-                else if (item == quoteChar && !escape)
-                {
-                    includeWhiteSpace = !includeWhiteSpace;
-                    continue;
-                }
-                else if (item == escapeChar && !escape)
-                {
-                    escape = true;
-                    continue;
-                }
-
-                arg += item;
-                escape = false;
-            }//foreach item
-
-            return arguments.ToArray();
-        }
     }
 
-    static class CmdLineToArgvW
-    {
-        // The previous examples on this page used incorrect
-        // pointer logic and were removed.
-
-        public static string[] SplitArgs(string unsplitArgumentLine)
-        {
-            int numberOfArgs;
-            IntPtr ptrToSplitArgs;
-            string[] splitArgs;
-
-            ptrToSplitArgs = CommandLineToArgvW(unsplitArgumentLine, out numberOfArgs);
-
-            // CommandLineToArgvW returns NULL upon failure.
-            if (ptrToSplitArgs == IntPtr.Zero)
-                throw new ArgumentException("Unable to split argument.", new Win32Exception());
-
-            // Make sure the memory ptrToSplitArgs to is freed, even upon failure.
-            try
-            {
-                splitArgs = new string[numberOfArgs];
-
-                // ptrToSplitArgs is an array of pointers to null terminated Unicode strings.
-                // Copy each of these strings into our split argument array.
-                for (int i = 0; i < numberOfArgs; i++)
-                    splitArgs[i] = Marshal.PtrToStringUni(
-                        Marshal.ReadIntPtr(ptrToSplitArgs, i * IntPtr.Size));
-
-                return splitArgs;
-            }
-            finally
-            {
-                // Free memory obtained by CommandLineToArgW.
-                LocalFree(ptrToSplitArgs);
-            }
-        }
-
-        [DllImport("shell32.dll", SetLastError = true)]
-        static extern IntPtr CommandLineToArgvW(
-            [MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine,
-            out int pNumArgs);
-
-        [DllImport("kernel32.dll")]
-        static extern IntPtr LocalFree(IntPtr hMem);
-    }
 }//namespace
--- EncodingConverter/Win32.cs
+++ EncodingConverter/Win32.cs
@@ -4,13 +4,127 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.ComponentModel;
 
 namespace EncodingConverter
 {
     static class Win32Helper
     {
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool AllocConsole();
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //[return: MarshalAs(UnmanagedType.Bool)]
+        //public static extern bool AllocConsole();
+
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //[return: MarshalAs(UnmanagedType.Bool)]
+        //public static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll",
+            EntryPoint = "GetStdHandle",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr GetStdHandle(int nStdHandle);
+        [DllImport("kernel32.dll",
+            EntryPoint = "AllocConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern int AllocConsole();
+        public const int STD_OUTPUT_HANDLE = -11;
+        public const int MY_CODE_PAGE = 437;
+
     }
+
+    public static class CmdLineToArgvW
+    {
+        // The previous examples on this page used incorrect
+        // pointer logic and were removed.
+
+        public static string[] SplitArgs(string unsplitArgumentLine)
+        {
+            int numberOfArgs;
+            IntPtr ptrToSplitArgs;
+            string[] splitArgs;
+
+            ptrToSplitArgs = CommandLineToArgvW(unsplitArgumentLine, out numberOfArgs);
+
+            // CommandLineToArgvW returns NULL upon failure.
+            if (ptrToSplitArgs == IntPtr.Zero)
+                throw new ArgumentException("Unable to split argument.", new Win32Exception());
+
+            // Make sure the memory ptrToSplitArgs to is freed, even upon failure.
+            try
+            {
+                splitArgs = new string[numberOfArgs];
+
+                // ptrToSplitArgs is an array of pointers to null terminated Unicode strings.
+                // Copy each of these strings into our split argument array.
+                for (int i = 0; i < numberOfArgs; i++)
+                    splitArgs[i] = Marshal.PtrToStringUni(
+                        Marshal.ReadIntPtr(ptrToSplitArgs, i * IntPtr.Size));
+
+                return splitArgs;
+            }
+            finally
+            {
+                // Free memory obtained by CommandLineToArgW.
+                LocalFree(ptrToSplitArgs);
+            }
+        }
+
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern IntPtr CommandLineToArgvW(
+            [MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine,
+            out int pNumArgs);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr LocalFree(IntPtr hMem);
+
+        public static string[] SplitArgsManaged(string line)
+        {
+            const char escapeChar = '\\';
+            const char quoteChar = '"';
+
+            List<string> arguments = new List<string>();
+
+            string arg = string.Empty;
+            bool includeWhiteSpace = false;
+            bool escape = false;
+
+            foreach (var item in line)
+            {
+                if (char.IsWhiteSpace(item))
+                {
+                    if (!includeWhiteSpace)
+                    {
+                        if (!string.IsNullOrEmpty(arg))
+                        {
+                            arguments.Add(arg);
+                        }
+                        arg = string.Empty;
+                        continue;
+                    }
+                }
+                else if (item == quoteChar && !escape)
+                {
+                    includeWhiteSpace = !includeWhiteSpace;
+                    continue;
+                }
+                else if (item == escapeChar && !escape)
+                {
+                    escape = true;
+                    continue;
+                }
+
+                arg += item;
+                escape = false;
+            }//foreach item
+
+            arguments.Add(arg);
+
+            return arguments.ToArray();
+        }
+
+    }
+
 }
